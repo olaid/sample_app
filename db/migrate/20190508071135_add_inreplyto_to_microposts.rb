910beea5458b3c79df6e6f2a9c94abcc101a58bf
olaid <olaid@hotmail.co.jp>
2019-05-11 18:05:15 +0900

    migration file

--- db/migrate/20190508071135_add_inreplyto_to_microposts.rb
+++ db/migrate/20190508071135_add_inreplyto_to_microposts.rb
@@ -1,5 +1,5 @@
 class AddInreplytoToMicroposts < ActiveRecord::Migration[5.2]
   def change
-    add_column :microposts, :in_reply_to, :string
+    add_column :microposts, :in_reply_to, :integer
   end
 end
